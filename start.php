3c39198de887f44bbda9debb52140a4754849151
Khalid Mohamad <khalidmohamad@web.de>
2024-06-28 14:46:50 +0200

    funktion part 1

--- start.php
+++ start.php
@@ -8,6 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tic Tac Toe</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+
 </head>
 
 <body>
